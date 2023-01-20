aebd0268bd5284eba689bf1f66be1655e417a4a9
benjamin giudice <ben.giudice@gmail.com>
2023-01-20 14:19:07 +0800

    Implement enemy elipse movement

--- src/enemy.rs
+++ src/enemy.rs
@@ -1,6 +1,7 @@
 use crate::components::{Enemy, FromEnemy, Laser, Movable, SpriteSize, Velocity};
 use crate::{
-    EnemyCount, GameTextures, WinSize, ENEMY_LASER_SIZE, ENEMY_MAX, ENEMY_SIZE, SPRITE_SCALE,
+    EnemyCount, GameTextures, WinSize, BASE_SPEED, ENEMY_LASER_SIZE, ENEMY_MAX, ENEMY_SIZE,
+    SPRITE_SCALE, TIME_STEP,
 };
 use bevy::ecs::schedule::ShouldRun;
 use bevy::prelude::*;
@@ -21,7 +22,8 @@ impl Plugin for EnemyPlugin {
             SystemSet::new()
                 .with_run_criteria(enemy_fire_criteria)
                 .with_system(enemy_fire_system),
-        );
+        )
+        .add_system(enemy_movement_system);
     }
 }
 
@@ -83,5 +85,33 @@ fn enemy_fire_system(
         .insert(FromEnemy)
         .insert(Movable { auto_despawn: true })
         .insert(Velocity { x: 0., y: -1. });
+    }
+}
+
+fn enemy_movement_system(time: Res<Time>, mut q: Query<&mut Transform, With<Enemy>>) {
+    let now = time.elapsed_seconds();
+    for mut transform in q.iter_mut() {
+        let (x_org, y_org) = (transform.translation.x, transform.translation.y);
+        let max_distance = TIME_STEP * BASE_SPEED;
+        let dir: f32 = -1.;
+        let (x_pivot, y_pivot) = (0., 0.);
+        let (x_radius, y_radius) = (200., 130.);
+        let angle = dir * BASE_SPEED * TIME_STEP * now % 360. / PI;
+        let x_dst = x_radius * angle.cos() + x_pivot;
+        let y_dst = y_radius * angle.sin() + y_pivot;
+        let dx = x_org - x_dst;
+        let dy = y_org - y_dst;
+        let distance = (dx * dx + dy * dy).sqrt();
+        let distance_ratio = if distance != 0. {
+            max_distance / distance
+        } else {
+            0.
+        };
+        let x = x_org - dx * distance_ratio;
+        let x = if dx > 0. { x.max(x_dst) } else { x.min(x_dst) };
+        let y = y_org - dy * distance_ratio;
+        let y = if dy > 0. { y.max(y_dst) } else { y.min(y_dst) };
+        let translation = &mut transform.translation;
+        (translation.x, translation.y) = (x, y);
     }
 }
